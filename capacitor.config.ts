dc3f8375bab85ca8514a30e7cac7d7cc504ce64c
William Hollywood <hollywwill@ecs.vuw.ac.nz>
2021-10-13 10:29:15 +1300

    using capcaitor for emulation on mobile

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wdh.app',
+  appName: 'TrackerMan',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
