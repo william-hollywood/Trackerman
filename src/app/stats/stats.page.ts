a43657a3e808f463144aabd55f3d92ac9463eafc
William Hollywood <hollywwill@ecs.vuw.ac.nz>
2021-10-14 23:37:29 +1300

    bar chart now populating

--- src/app/stats/stats.page.ts
+++ src/app/stats/stats.page.ts
@@ -1,5 +1,8 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+import { SettingsPage } from '../settings/settings.page';
 
 @Component({
   selector: 'app-stats',
@@ -12,49 +15,84 @@ export class StatsPage implements AfterViewInit {
 
   barChart: any;
 
-  constructor() {}
+  constructor() { }
 
   ngAfterViewInit() {
     this.barChartMethod();
   }
 
-  barChartMethod() {
-    this.barChart = new Chart(this.barCanvas.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
-        datasets: [{
-          label: '# of Votes',
-          data: [200, 50, 30, 15, 20, 34],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
+  doRefresh(event) {
+    this.barChartMethod();
+    event.target.complete();
   }
 
+  barChartMethod() {
+    if (SettingsPage.valid) {
+      let now = Date.now();
+      let time = now - 604800000 + 86400000;
+      let labelData = [] // array strings, of day starting from 6 days ago until today
+      for (let i = 0; i < 7; i++) {
+        let d = new Date(time);
+        let str = d.toLocaleDateString();
+        labelData.push(str.substr(0, str.length - 5))
+        time += 86400000;
+      }
+      let pastData = [0,0,0,0,0,0,0]; // array numbers, total on each day of past week.
+
+      firebase.database().ref("routes/" + SettingsPage.uname + "/").get().then((val) => {
+        if (val.toJSON() != null) {
+          val.forEach((route) => {
+            if (parseInt(route.key)*1000 > now - 604800000 + 86400000){
+              let d = new Date(parseInt(route.key)*1000);
+              let str = d.toLocaleDateString();
+              str = str.substr(0, str.length - 5)
+              for (let i = 0; i < 7; i++){
+                if (str == labelData[i]){
+                  pastData[i] += route.child("distance").val();
+                }
+              }
+            }
+          });
+          this.barChart = new Chart(this.barCanvas.nativeElement, {
+            type: 'bar',
+            data: {
+              labels: labelData,
+              datasets: [{
+                label: 'Distance Traveled (Km)',
+                data: pastData,
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+              }]
+            },
+            options: {
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }]
+              }
+            }
+          });
+        }
+      });
+
+      
+    }
+  }
 }
