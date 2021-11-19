a7991a59718d3c7a4e3c0653191c5e17c1dfda0f
William Hollywood <Will@oceanplait.com>
2021-11-20 11:24:39 +1300

    Redacted info in firebaseConfig. (yes you can go back in the commits. but the api key is no longer valid so it doesnt matter)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrQZ-TtxyW3K9HfSx2MCeAVu9eQ_XE0Z8",    
-    authDomain: "swen325-trackerman.firebaseapp.com",    
-    databaseURL: "https://swen325-trackerman-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "swen325-trackerman",    
-    storageBucket: "swen325-trackerman.appspot.com",    
-    messagingSenderId: "960313101926",    
-    appId: "1:960313101926:web:54593a603800f74df667a7",
-    measurementId: "G-HJLMCSM0W6"
+    apiKey: "REDACTED",    
+    authDomain: "REDACTED",    
+    databaseURL: "REDACTED",
+    projectId: "REDACTED",    
+    storageBucket: "REDACTED",    
+    messagingSenderId: "REDACTED",    
+    appId: "REDACTED",
+    measurementId: "REDACTED"
   };
 
 @Component({
